7d8a5f26e8fe714c21970c095bf82ebd0d0255b0
Romain Dumon <ardumon@gmail.com>
2018-02-23 13:07:36 +0000

    push

---
+++ impact/moustache.py
@@ -0,0 +1,142 @@
+import sys
+import psycopg2
+import decimal
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+plotly.tools.set_credentials_file(username='RomainDumon', api_key='4W9SerJqoMLaCR8e1abJ')
+
+
+DB_NAME='osmlondon'
+DB_USER='romaindumon'
+DB_PWD=''
+DB_HOST=''
+DB_PORT='5432'
+
+NODE_TYPE="Nodes"
+WAY_TYPE="Ways"
+RELATION_TYPE="Relations"
+
+class DB(object):
+    """encaspulate a database connection."""
+
+    def __init__(self):
+        try:
+            self.connection = psycopg2.connect("dbname='"+DB_NAME+"' user='"+DB_USER+"' password='"+DB_PWD+"' host='"+DB_HOST+"' port='"+DB_PORT+"'")
+        except:
+            print('\033[91m'+"Unable to connect to the database."+'\033[0m')
+            sys.exit(-1)
+        print("Connected to db!") 
+
+    def execute(self,commands=[]):
+
+        try:
+            cur = self.connection.cursor()
+            # create table one by one
+            for command in commands:
+                cur.execute(command)
+
+            result = cur.fetchall()
+
+            # close communication with the PostgreSQL database server
+            cur.close()
+            # commit the changes
+            self.connection.commit()
+            return result
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            print('\033[91m'+"\nSQL ERROR:\n"+str(error)+'\033[0m')
+            sys.exit(-1)
+
+
+if __name__ == '__main__':
+
+	db = DB()
+	#Average number of edits per user per week for the six months before
+	#This query is location proof
+	expected_per_user = db.execute(["with C as((SELECT count(*) as contributions, user_name from nodes where created_at >= '2008-10-01' AND created_at < '2009-04-01' AND latitude > 2414900 AND longitude > 514749580 AND latitude < -25770000 AND longitude < 515409360 GROUP BY user_name)UNION ALL (SELECT count(*) as contributions, user_name from ways where created_at >= '2008-10-01' AND created_at < '2009-04-01' GROUP BY user_name) UNION ALL (SELECT count(*) as contributions, user_name from relations where created_at >= '2008-10-01' AND created_at < '2009-04-01' GROUP BY user_name)) SELECT (SUM(contributions)/26) as contributions, user_name from C GROUP BY user_name ORDER BY SUM(contributions)"])
+
+	# build a dictionary with those results
+	expected = {}
+
+	#list of all user in each group
+	group1 = []
+	group2 = []
+	group3 = []
+	group4 = []
+	group5 = []
+	
+	for a in expected_per_user:
+		if a[0] > 0.0 and a[0] < 0.4:
+			group1.append(a[1])
+		if a[0] > 0.4 and a[0] < 4.0:
+			group2.append(a[1])
+		if a[0] > 4.0 and a[0] < 40.0:
+			group3.append(a[1])
+		if a[0] > 40.0 and a[0] < 400.0:
+			group4.append(a[1])
+		if a[0] > 400.0 and a[0] < 4000.0:
+			group5.append(a[1])
+		expected[a[1]] = a[0]
+
+	groups = []
+	groups.append(group1)
+	groups.append(group2)
+	groups.append(group3)
+	groups.append(group4)
+	groups.append(group5)
+
+
+	#query average contribution for each user in each group
+	actual_one_month = {}
+	actual_per_user_one_month = db.execute(["with C as((SELECT count(*) as contributions, user_name from nodes where created_at >= '2009-04-01' AND created_at < '2009-05-01' AND latitude > 2414900 AND longitude > 514749580 AND latitude < -25770000 AND longitude < 515409360 GROUP BY user_name)UNION ALL (SELECT count(*) as contributions, user_name from ways where created_at >= '2009-04-01' AND created_at < '2009-05-01' GROUP BY user_name) UNION ALL (SELECT count(*) as contributions, user_name from relations where created_at >= '2009-04-01' AND created_at < '2009-05-01' GROUP BY user_name)) SELECT (SUM(contributions)/4) as contributions, user_name from C GROUP BY user_name ORDER BY SUM(contributions)"])
+	for a in actual_per_user_one_month:
+		actual_one_month[a[1]] = a[0]
+		print(actual_one_month[a[1]])
+
+	abnormalReturnPerUser_one_month = {}
+	for group in groups:
+		for user in group:
+			#print(actual_one_month.get(user,decimal.Decimal(0.0)))
+			#print(expected.get(user,decimal.Decimal(0.0)))
+			abnormalReturnPerUser_one_month[user] = actual_one_month.get(user,decimal.Decimal(0.0)) - expected.get(user,decimal.Decimal(0.0))
+
+	dataAbnormal = []
+	for group in groups:
+		data = []
+		for user in group:
+			data.append(abnormalReturnPerUser_one_month[user])
+		dataAbnormal.append(data)
+
+	trace0 = go.Box(
+    	y=dataAbnormal[0]
+	)
+	trace1 = go.Box(
+    	y=dataAbnormal[1]
+	)
+	trace2 = go.Box(
+    	y=dataAbnormal[2]
+	)
+	trace3 = go.Box(
+    	y=dataAbnormal[3]
+	)
+	trace4 = go.Box(
+    	y=dataAbnormal[4]
+	)
+
+	data = [trace0,trace1,trace2,trace3,trace4]
+	py.plot(data)
+
+
+
+
+
+
+
+		
+
+
+
+
+
+
