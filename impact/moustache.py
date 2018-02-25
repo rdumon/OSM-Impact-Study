64c722575f366084c7b1d5f7d5645aa26d22a5e9
Romain Dumon <ardumon@gmail.com>
2018-02-25 10:19:02 +0000

    created function

--- impact/moustache.py
+++ impact/moustache.py
@@ -6,11 +6,6 @@ from impact_analyzer import *
 sys.path.insert(0, '../lib/')
 from db import DB
 
-import plotly
-import plotly.plotly as py
-import plotly.graph_objs as go
-plotly.tools.set_credentials_file(username='RomainDumon', api_key='4W9SerJqoMLaCR8e1abJ')
-
 NODE_TYPE="Nodes"
 WAY_TYPE="Ways"
 RELATION_TYPE="Relations"
@@ -27,7 +22,9 @@ config = {
 if __name__ == '__main__':
 
 	db = DB(config)
-	
+	abnormal_return_for_group(db,2,3,2)
+
+
 
 
 
