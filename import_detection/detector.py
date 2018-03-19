094d7bdfcf29ac7840c234ff4dc37e62617308be
JhumanJ <julien@nahum.net>
2018-03-19 18:00:45 +0000

    Detect ways

--- import_detection/detector.py
+++ import_detection/detector.py
@@ -24,16 +24,15 @@ from lib.amenities import *
 # X is the lower left point
 # Y is the upper right point
 
-def detectImport(db,cityName='', x = None , y = None):
-
-    # If there is a location restriction
-    where_clause = ''
-    if x!=None and y!=None and len(x) == 2 and len(y) == 2:
-        where_clause = 'where (latitude > '+str(x[1])+' and longitude > '+str(x[0])+') and (latitude < '+str(y[1])+' and longitude < '+str(y[0])+')'
+def detectImport(db,cityName='', x = None , y = None, dir_write_to = '', googleDriveConnection = '', detectionLevel = 20):
 
     # Queries
-    creation_date_query = 'select user_name, to_char(min(created_at),\'YYYYMMDD\') as created_at from nodes '+where_clause+' group by user_name'
-    all_contribution_query = 'select to_char(created_at,\'YYYYMMDD\') as created_at, user_name, count(*) as contrib from nodes '+where_clause+' group by user_name, to_char(created_at,\'YYYYMMDD\')'
+    creation_date_query = "SELECT user_name, min(created_at) from (select user_name,to_char(min(created_at),'YYYYMMDD') as created_at from nodes  group by user_name UNION ALL select user_name, to_char(min(created_at),'YYYYMMDD') as created_at from ways group by user_name) as A group by user_name"
+
+    all_contribution_node_query = 'select to_char(created_at,\'YYYYMMDD\') as created_at, user_name, count(*) as contrib from nodes group by user_name, to_char(created_at,\'YYYYMMDD\')'
+    all_contribution_way_query = 'select to_char(created_at,\'YYYYMMDD\') as created_at, user_name, count(*) as contrib from ways group by user_name, to_char(created_at,\'YYYYMMDD\')'
+
+    all_contribution_query = 'SELECT created_at, user_name, SUM(A.contrib) as contrib FROM ( '+ all_contribution_node_query+' UNION ALL '+ all_contribution_way_query+') as A group by user_name,created_at ;'
 
     # Get dictionnary of birthdate of each user
     temp_all_birthday = db.execute([creation_date_query]);
@@ -100,7 +99,7 @@ def detectImport(db,cityName='', x = None , y = None):
 
     average_new_user = sum(daily_best_new_user_contrib['value']) / len(daily_best_new_user_contrib['value'])
     average = sum(daily_best_contrib['value']) / len(daily_best_contrib['value'])
-    import_limit = average * 20
+    import_limit = average * detectionLevel
 
     # Now we plot all graphs
     trace0 = go.Scatter(
@@ -135,137 +134,32 @@ def detectImport(db,cityName='', x = None , y = None):
         name = "Import Limit"
     )
 
-    data_graph = [trace0, trace1,trace2,trace3,trace4]
+    data_graph = [trace2,trace3,trace0,trace1,trace4]
 
     # py.plot(data_graph, filename=('Import Detection '+cityName))
 
+    layout = go.Layout(title = "Import Detection "+cityName,width=1200, height=540)
+
+    data = [trace2,trace3,trace0,trace1,trace4]
+    fig = dict(data = data, layout = layout)
+    # py.plot(data,filename='box-plots osm London month')
+    filelocation = dir_write_to['local']+'/import-dectection.png'
+    py.image.save_as(fig,filename = filelocation)
+
+    # UPLOAD TO GOOGLE DRIVE
+    filename = 'import-dectection.png'
+    googleDriveConnection.upload_GoogleDrive(filename,filelocation, dir_write_to['google'])
+
     detected_imports = []
     for i in range(0,len(daily_best_contrib['value'])):
-        print(str(daily_best_contrib['value'][i]) + ' vs '+ str(import_limit))
         if daily_best_contrib['value'][i] >= import_limit:
             detected_imports.append([daily_best_contrib['date'][i],daily_best_contrib['user'][i]])
 
     return detected_imports
 
-def detectWaysImport(db,cityName='',x=None,y=None):
-
-    # If there is a location restriction
-    where_clause = ''
-    ways_where_clause = ''
-    if x!=None and y!=None and len(x) == 2 and len(y) == 2:
-        where_clause = 'where (latitude > '+str(x[1])+' and longitude > '+str(x[0])+') and (latitude < '+str(y[1])+' and longitude < '+str(y[0])+')'
-        ways_where_clause = ' and count(case when (latitude > '+str(x[1])+' and longitude > '+str(x[0])+') and (latitude < '+str(y[1])+' and longitude < '+str(y[0])+') then 0 end) > 0 '
-
-    # Queries
-    ways_query = 'select count(ways.id), user_name from ways_nodes JOIN ways ON ways.id = ways_nodes.id AND ways.version = ways_nodes.version'
-    ways_query += ' group by user_name having ( count(*)>1' + ways_where_clause +' )'
-
-    # TODO: add group by date
-
-    print(ways_query)
-    sys.exit(-1)
-
-    creation_date_query = 'select user_name, to_char(min(created_at),\'YYYYMMDD\') as created_at from nodes '+where_clause+' group by user_name'
-    all_contribution_query = 'select to_char(created_at,\'YYYYMMDD\') as created_at, user_name, count(*) as contrib from nodes '+where_clause+' group by user_name, to_char(created_at,\'YYYYMMDD\')'
-
-    # Get dictionnary of birthdate of each user
-    temp_all_birthday = db.execute([creation_date_query]);
-    all_birthday = {}
-    for item in temp_all_birthday:
-        all_birthday[item[0]] = item[1]
-
-    # Get daily contribution of each user
-    all_contribution = db.execute([all_contribution_query]);
-    all_contribution = sorted(all_contribution, key=itemgetter(0))
-
-    # Graph of best user
-    daily_best_contrib = {'value':[], 'date':[], 'user':[]}
-    daily_max = all_contribution[0][2]
-    daily_date = all_contribution[0][0]
-    daily_user = all_contribution[0][1]
-    for day_contrib in all_contribution:
-        if day_contrib[0]==daily_date:
-            if day_contrib[2] > daily_max:
-                daily_max = day_contrib[2]
-                daily_user = day_contrib[1]
-        else:
-            daily_best_contrib['value'].append(daily_max)
-            daily_best_contrib['date'].append(datetime.datetime.strptime(daily_date, "%Y%m%d"))
-            daily_best_contrib['user'].append(daily_user)
-            daily_max = day_contrib[2]
-            daily_user = day_contrib[1]
-            daily_date = day_contrib[0]
-
-    # Construct array of first contrib foe each user and their value
-    data = []
-    for day_contrib in all_contribution:
-        if all_birthday[day_contrib[1]] == day_contrib[0]:
-            data.append(day_contrib)
-
-    # Sort data
-    data = sorted(data, key=itemgetter(0))
-
-    # Now we only keep the best for each day
-    # ------- Graphs of best new user contribution --------
-    current_date = data[0][0]
-    current_max = data[0][2]
-    current_max_user = data[0][1]
-
-    daily_best_new_user_contrib = {'value':[], 'date':[], 'user':[]}
-
-    graph_date = []
-    graph_val = []
-    graph_text = []
-    for item in data:
-        if item[0] == current_date:
-            if item[2] > current_max:
-                current_max = item[2]
-                current_max_user = item[1]
-        else:
-            daily_best_new_user_contrib['date'].append(datetime.datetime.strptime(current_date, "%Y%m%d"))
-            daily_best_new_user_contrib['value'].append(current_max)
-            daily_best_new_user_contrib['user'].append(current_max_user)
-
-            current_date = item[0]
-            current_max = item[2]
-            current_max_user = item[1]
-
-    # Now we plot all graphs
-    trace0 = go.Scatter(
-        x = daily_best_new_user_contrib['date'],
-        y = daily_best_new_user_contrib['value'],
-        text = daily_best_new_user_contrib['user'],
-        name = "Daily best new user contribution"
-    )
-
-    trace1 = go.Scatter(
-        x = daily_best_new_user_contrib['date'],
-        y = [sum(daily_best_new_user_contrib['value']) / len(daily_best_new_user_contrib['value'])] * len(daily_best_new_user_contrib['date']),
-        name = "Average daily best new user contribution"
-    )
-
-    trace2 = go.Scatter(
-        x = daily_best_contrib['date'],
-        y = daily_best_contrib['value'],
-        text = daily_best_contrib['user'],
-        name = "Daily best contribution"
-    )
-
-    trace3 = go.Scatter(
-        x = daily_best_contrib['date'],
-        y = [sum(daily_best_contrib['value']) / len(daily_best_contrib['value'])] * len(daily_best_contrib['value']),
-        name = "Average daily best user contribution"
-    )
-
-    data_graph = [trace0, trace1,trace2,trace3]
-
-    py.plot(data_graph, filename=('Import Ways Detection '+cityName))
-
-
-
 def find_contribution_type_of_import(db, iMport = [], table =""):
 
-    user_name = iMport[1] 
+    user_name = iMport[1]
     date_convert = iMport[0]
 
     query = "SELECT json_agg(tags) as tags, id FROM "+table+" WHERE created_at > '" + date_convert.strftime('%Y-%m-%d') + " 00:00:00' AND created_at < '" + date_convert.strftime('%Y-%m-%d') + " 24:00:00' AND user_name = '"+user_name+"' GROUP BY id  "
@@ -323,13 +217,3 @@ def imports_report(db, imports= []):
         imports_information.append(array)
         array = []
     return imports_information
-    
-
-
-
-
-
-
-
-
-
--- import_detection/general.py
+++ import_detection/general.py
@@ -33,13 +33,13 @@ def analyse_import(db, googleDriveConnection, iMport, x = None, y = None, city =
 
 	# # FOR EACH ANALYSIS WE WANT TO LOOK AT impact afer 1 week, 1 month, 3 month
 	time_intervals = [import_date+relativedelta(weeks=+1), import_date+relativedelta(months=+1), import_date+relativedelta(months=+3)]
-	
+
 	# # ABNORMAL RETURN OF CONTRIBUTIONS PER GROUP (based on expected return of 6 month before the import)
 	# # 1 week, 1 month, 3 month
 	for date_after in time_intervals:
 		abnormal_return_for_group(db, googleDriveConnection, groups, import_date-relativedelta(months=+6) , import_date , date_after, x, y, import_dir)
 
-	# EVOLUTION OF EDITS PERIOD 
+	# EVOLUTION OF EDITS PERIOD
 	# 6 month after
 	contribution_types_gobal_analysis(db, googleDriveConnection, import_date-relativedelta(months=+6),import_date,import_date+relativedelta(months=+6), x, y, import_dir)
 
@@ -49,24 +49,21 @@ def analyse_import(db, googleDriveConnection, iMport, x = None, y = None, city =
 	for date_after in time_intervals:
 		impact_import_creationtomaintenance_ratio_abnormal_return(db, googleDriveConnection, groups,  import_date-relativedelta(months=+6), import_date, date_after, import_dir)
 
-<<<<<<< HEAD
 	# # # AMENITY EVOLUTION PER GROUP
 	# # # 1 week, 1 month, 3 month
 	# for x in range(0,3):
 	# top_amenity_evolution_per_group(db,groups, iMport, date_before,event_date,date_after, x=None, y=None)
-=======
+
 	# # AMENITY EVOLUTION PER GROUP
 	# # 1 week, 1 month, 3 month
 	for date_after in time_intervals:
 		top_amenity_evolution_per_group(db,googleDriveConnection, import_date-relativedelta(months=+6),import_date,date_after, iMport, x, y, import_dir)
->>>>>>> d17ff832b7daa31cdb434fb7e6c40e1b11db801b
 
 	# # SURVIVAL ANALISIS
-	# # run the survival analysis for the 	
+	# # run the survival analysis for the
 
 
 
 	#Keeping track of each graph ids and their corresponding imports (look into tacking screenshots for each graph we produce)
 	#Compare to well chosen Control Groups !!!!!!!!!!!!
 	#this function should return information helping us decipher quickly what graphs correspond to which imports
-	
